b585163289cae9a90ac7926931b1adaf96faa9c9
FrankRdgz <frank.rdgz7@gmail.com>
2023-02-02 20:10:19 -0500

    fixed coin flip
    also modifeded maps

--- js/coinFlip.js
+++ js/coinFlip.js
@@ -1,21 +1,39 @@
+let btnFlip = document.getElementById("flip");
+let btnStop = document.getElementById("stop");
+
+
 var angle = 0;
 var flip = false;
 
-function startFlip() {
+btnFlip.addEventListener("click", function() {
   flip = true;
-  rotateCoin();
-}
+  rotateCoin();  
+  this.style.display="none";
+  flipResult.style.display="none";
+  btnStop.style.display = "inline-block";
+});
 
-function stopFlip() {
+btnStop.addEventListener("click", function() {
   flip = false;
+  this.style.display = "none";
+  btnFlip.style.display = "inline-block";
+  flipResult.style.display="inline-block";
+
   var result = Math.random() < 0.5 ? "heads" : "tails";
   document.getElementById("coin").src ="./images/"+ result + ".jpeg";
-}
+  flipResult.innerHTML = `<h1> ${result}! </h1>`
+});
 
 function rotateCoin() {
   if (flip) {
     angle += 180;
     document.getElementById("coin").style.transform = "rotateY(" + angle + "deg)";
-    setTimeout(rotateCoin, 400);
+    if(angle%360 === 0){
+      document.getElementById("coin").src ="./images/tails.jpeg";
+    }
+    else{
+      document.getElementById("coin").src ="./images/heads.jpeg";
+    }
+    setTimeout(rotateCoin, 100);
   }
 }
