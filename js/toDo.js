43978ebf7faeeacdcb73ff78a94e8d2a51a7f427
FrankRdgz <frank.rdgz7@gmail.com>
2023-02-08 01:01:10 -0500

    local storage for toto

--- js/toDo.js
+++ js/toDo.js
@@ -1,3 +1,4 @@
+/*
 form = document.querySelector("form");
 taskList = document.querySelector("#task-list")
 
@@ -46,4 +47,122 @@ form.addEventListener('submit', e => {
     
   });
 
-  
+  */
+
+const todoForm = document.querySelector('#todo__form')
+const todoList = document.querySelector('.todos')
+const totalTasks = document.querySelector('#total__tasks')
+const completedTasks = document.querySelector('#completed__tasks')
+const remainingTasks = document.querySelector('#remaining__tasks')
+const mainInput = document.querySelector('#todo__form input')
+
+let tasks = JSON.parse(localStorage.getItem('tasks')) || []
+
+if(localStorage.getItem('tasks')){
+    tasks.map((task) => {
+        createTask(task)
+    })
+}
+
+todoForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const inputValue = mainInput.value
+
+    if(inputValue == ""){
+        return
+    }
+
+    const task = {
+        id: new Date().getTime(),
+        name: inputValue,
+        isCompleted: false
+    }
+
+    tasks.push(task)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+
+    createTask(task)
+
+    todoForm.reset()
+    mainInput.focus()
+})
+
+todoList.addEventListener('click', (e) => {
+    if(e.target.classList.contains('remove__task') || e.target.parentElement.classList.contains('remove__task') || e.target.parentElement.parentElement.classList.contains('remove__task')){
+        const taskId = e.target.closest('li').id
+        removeTask(taskId)  
+    }
+})
+
+todoList.addEventListener('input', (e) => {
+    const taskId = e.target.closest('li').id
+    updateTask(taskId, e.target)
+})
+
+
+function createTask(task){
+    const taskEl = document.createElement('li')
+    taskEl.setAttribute('id', task.id)
+
+    if(task.isCompleted){
+        taskEl.classList.add('complete')
+    }
+
+    const taskElMarkup = `
+    <div>
+        <input type="checkbox" name="tasks" id="${task.id}" ${task.isCompleted ? 'checked' : ''}> 
+        <span ${!task.isCompleted ? 'contenteditable' : ''}>${task.name}</span>
+    </div>
+
+    <button title="Remove the '${task.name}' task" class="remove__task">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgb(71, 71, 71);"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg>
+    </button>
+    `
+
+    taskEl.innerHTML = taskElMarkup
+    todoList.appendChild(taskEl)
+
+    countTasks()
+}
+
+function countTasks(){
+    const completedTasksArray = tasks.filter((task) => task.isCompleted === true)
+
+    totalTasks.textContent = tasks.length
+    completedTasks.textContent = completedTasksArray.length
+    remainingTasks.textContent = tasks.length - completedTasksArray.length
+}
+
+function removeTask(taskId){
+    tasks = tasks.filter((task) => task.id !== parseInt(taskId))
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    document.getElementById(taskId).remove()
+    countTasks()
+}
+
+function updateTask(taskId, el){
+    const task = tasks.find((task) => task.id === parseInt(taskId))
+
+    if(el.hasAttribute('conteteditable')){
+        task.name = el.textContent
+    }
+    else{
+        const span = el.nextElementSibiling
+        const parent = el.closest('li')
+
+        task.isCompleted = !task.isCompleted
+
+        if(task.isCompleted){
+            span.removeAttribute('contenteditable')
+            parent.classList.add('complete')
+        }
+        else{
+            span.setAttribute('contenteditable', 'true')
+            parent.classList.remove('complete')
+        }
+    }
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    countTasks()
+}
--- js/toDoCred.js
+++
@@ -1,9 +0,0 @@
-const username = document.querySelector('#username').value;
-const password = document.querySelector('#pass').value;
-var myName = "frank";
-var tempPass = "123"
-
-var btn = document.getElementById("tempLogin");
-btn.addEventListener("click", function(){
-    window.location.href = "./to-do.html";
-});
